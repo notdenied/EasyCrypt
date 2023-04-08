342ec3aaa02cf8d5b00827679ecf5e199ec9e230
Андрей Рыжов <ryzhov.andrey.a@gmail.com>
2023-04-08 03:56:35 +0300

    First release!

---
+++ console.py
@@ -0,0 +1,95 @@
+import sys
+
+from constants import help_message, OPERATIONS, LANGUAGES, CIPHERS
+from ciphers import *
+from steganography import *
+
+args_ = sys.argv[1:]
+
+if not args_ or '--help' in args_:
+    print(help_message)
+    exit()
+
+args = {'lang': 'ru'}
+
+for arg in args_:
+    key, value = arg.split('=')
+    args[key[2:]] = value
+
+if 'mode' not in args:
+    print('Error: mode not passed.')
+    exit()
+
+if args['mode'] == 'cipher':
+    if 'text' not in args and 'file' not in args:
+        print('Error: no input given.')
+        exit()
+    if 'text' in args:
+        text = args['text']
+    else:
+        with open(args['file'], 'r', encoding='utf-8') as f:
+            text = f.read()
+    op = args.get('op')
+    if op not in OPERATIONS:
+        print('Wrong operation chosen!')
+        exit()
+    if args['lang'] not in LANGUAGES:
+        print('Wrong language chosen!')
+        exit()
+    lang = args['lang']
+    cip = args.get('cipher')
+    if cip not in CIPHERS:
+        print('Wrong cipher chosen!')
+        exit()
+    if op == 'Encode':
+        if cip == 'Caesar':
+            mes = Caesar.encode(text, lang, int(key))
+        else:
+            if not key:
+                print('No key passed!')
+                exit()
+            if cip == 'Vigenere':
+                mes = Vigenere.encode(text, lang, key)
+            elif cip == 'Vernam':
+                mes = Vernam.encode(text, key)
+    else:
+        if cip == 'Caesar':
+            if 'use_cracker' in args and args['use_cracker'] == 'true':
+                mes = f'Frequency cracker output:\n{Caesar.crack_by_frequency(text, lang)}\n\nEnchant cracker output:\n{Caesar.crack_by_enchant(text, lang)}'
+            else:
+                mes = Caesar.decode(text, lang, int(key))
+        else:
+            if not key:
+                raise ValueError('No key passed!')
+            if cip == 'Vigenere':
+                mes = Vigenere.decode(text, lang, key)
+            elif cip == 'Vernam':
+                mes = Vernam.decode(text, key)
+    print(mes)
+else:
+    in_path = args.get('input_path')
+    out_path = args.get('output_path')
+    op = args.get('op')
+    if op not in OPERATIONS:
+        print('Wrong operation chosen!')
+        exit()
+    if op == 'Encode':
+        if 'text' not in args and 'file' not in args:
+            print('Error: no input given.')
+            exit()
+        if 'text' in args:
+            text = args['text']
+        else:
+            with open(args['file'], 'r', encoding='utf-8') as f:
+                text = f.read()
+        if not in_path or not out_path:
+            print('output_path or input_path not passed!')
+            exit()
+        mes = 'Successfully inserted!'
+        Steganography.insert_text(in_path, text, out_path)
+    else:
+        if not in_path:
+            print('input_path not passed!')
+            exit()
+        mes = Steganography.read_text(in_path)
+    print(mes)
---
+++ constants.py
@@ -0,0 +1,36 @@
+
+
+RU_ALP = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+RU_IND = {RU_ALP[i]: i for i in range(len(RU_ALP))}
+RU_LEN = len(RU_ALP)
+RU_TOP = ['о', 'е', 'а', 'и', 'н']
+
+EN_ALP = 'abcdefghijklmnopqrstuvwxyz'
+EN_IND = {EN_ALP[i]: i for i in range(len(EN_ALP))}
+EN_LEN = len(EN_ALP)
+EN_TOP = ['e', 't', 'a', 'o', 'i']
+
+assert (RU_LEN == 33)
+assert (EN_LEN == 26)
+
+
+DEFAULT_W_SIZE = "800x800"
+WINDOW_TITLE = 'EasyCrypt — simple app for ciphers/steganography.'
+LANGUAGES = ['ru', 'en']
+CIPHERS = ['Caesar', 'Vigenere', 'Vernam']
+OPERATIONS = ["Encode", 'Decode']
+
+help_message = '''Usage: python3 console.py --arg=value
+
+Possible args:
+- mode: cipher / steganography - working mode.
+- cipher: Caesar / Vigenere / Vernam - cipher to use.
+- key: str - key or rotation for the cipher.
+- op: decode / encode - operation to process.
+- text: str - input text.
+- lang: ru / en - language to use, default - ru.
+- file [optional]: path - read input text from file.
+- use_cracker: true / false - use automatic Caesar cracking functions.
+- input_path: path - input image path.
+- output_path: path - output image path.
+'''
